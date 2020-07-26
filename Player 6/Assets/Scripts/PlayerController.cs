ccfadf5a59af7b660206884a6661bbe4cb7e9c18
Daimee MacIsaac <gingah.ninja47@gmail.com>
2020-07-25 22:44:58 -0400

    Unity Project created, programmed core mechanics

---
+++ Player 6/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,121 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerController : MonoBehaviour
+{
+    public float moveSpeed;
+    public float sprintMult;
+    public float mouseSensitivity;
+    public float jumpForce;
+
+    public Transform gunArm;
+
+    public GameObject bullet;
+
+    private float moveMod;
+
+    private Vector2 moveVal;
+    private Vector2 lookVal;
+
+    private Rigidbody rb;
+
+    // private Weapon[] weapons;
+    private Transform bulletSpawn;
+
+    private PlayerInput pIn;
+
+    private bool canJump;
+
+    public void OnMove(InputValue value)
+    {
+        moveVal = value.Get<Vector2>();
+    }
+
+    public void OnLook(InputValue value)
+    {
+        lookVal = value.Get<Vector2>();
+    }
+
+    public void OnJump(InputValue value)
+    {
+        Jump();
+    }
+
+    public void OnFire(InputValue value)
+    {
+        Shoot();
+    }
+
+    public void OnSprintStart(InputValue value)
+    {
+        moveMod = sprintMult;
+    }
+
+    public void OnSprintRelease(InputValue value)
+    {
+        moveMod = 1.0f;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        moveMod = 1.0f;
+        rb = GetComponent<Rigidbody>();
+        pIn = GetComponent<PlayerInput>();
+        canJump = true;
+        bulletSpawn = gunArm.Find("Gun").Find("BulletSpawn");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(0, lookVal.x * mouseSensitivity, 0);
+        gunArm.Rotate(lookVal.y * mouseSensitivity * -1, 0, 0);
+        if (gunArm.localRotation.eulerAngles.x > 70 && gunArm.localRotation.eulerAngles.x <= 180)
+        {
+            gunArm.localRotation = Quaternion.Euler(70, 0, 0);
+        }
+        else if (gunArm.localRotation.eulerAngles.x < 290 && gunArm.localRotation.eulerAngles.x >= 180)
+        {
+            gunArm.localRotation = Quaternion.Euler(290, 0, 0);
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        Vector3 move = (transform.forward * moveVal.y + transform.right * moveVal.x) * moveSpeed * moveMod;
+        move.y = rb.velocity.y;
+        rb.velocity = move;
+    }
+
+    private void Jump()
+    {
+        if (canJump)
+        {
+            rb.AddForce(new Vector3(0, jumpForce, 0));
+            canJump = false;
+        }
+    }
+
+    private void Shoot()
+    {
+        Instantiate(bullet, bulletSpawn.position, bulletSpawn.rotation);
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.collider.CompareTag("Ground"))
+        {
+            canJump = true;
+        }
+    }
+}
+
+public struct Weapon
+{
+    bool isGun;
+    GameObject prefab;
+    GameObject projectile;
+}
---
+++ Player 6/Assets/Scripts/Projectile.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Projectile : MonoBehaviour
+{
+    public float maxRange;
+    public float moveSpeed;
+
+    private float dist;
+    private Vector3 moveDir;
+    private Vector3 prevPos;
+    private Rigidbody rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        moveDir = transform.forward;
+
+        rb = GetComponent<Rigidbody>();
+        rb.velocity = moveDir * moveSpeed;
+        prevPos = transform.position;
+
+        dist = 0;
+    }
+
+    private void LateUpdate()
+    {
+        dist += Vector3.Distance(prevPos, transform.position);
+        prevPos = transform.position;
+        if (dist > maxRange)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        Hit(collision.collider.gameObject);
+    }
+
+    private void Hit(GameObject target)
+    {
+        // Implement code to damage players (and objects?)
+        Destroy(gameObject);
+    }
+}
